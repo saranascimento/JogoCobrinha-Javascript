5aa24459592ffa18bcac969c8838fe7077c7a9b2
Sara Gomes <sarah.gomes80@hotmail.com>
2020-03-16 23:55:42 -0300

    Definir fim do jogo

--- script.js
+++ script.js
@@ -50,6 +50,14 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
 
+    for(i = 1; i < snake.length; i++) {
+        // se a cobrinha se chocar com o proprio corpo, finaliza o jogo
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo) 
+            alert('Game Over ;(' )
+        }
+    }
+
     criarBG()
     criarCobrinha()
     drawFood() 
